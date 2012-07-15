611d491ea65d7a1f995dd498e0ccf9d45a8228a8
mkawick <mkawick@yahoo.com>
2012-07-15 11:46:00 -0700

    bettererrorchecking

--- Source/AssetLoadManager.cpp
+++ Source/AssetLoadManager.cpp
@@ -377,22 +377,32 @@ void AssetObject::RecursiveRender (const struct aiScene *sc, const struct aiNode
 	glMultMatrixf((float*)&m);
 
 	// draw all meshes assigned to this node
-	for (; n < nd->mNumMeshes; ++n) {
+	for (; n < nd->mNumMeshes; ++n) 
+	{
 		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+		if( mesh == NULL )
+			continue;
 
 		ApplyMaterial(sc->mMaterials[mesh->mMaterialIndex]);
 
-		if(mesh->mNormals == NULL) {
+		if(mesh->mNormals == NULL) 
+		{
 			glDisable(GL_LIGHTING);
-		} else {
+		} 
+		else 
+		{
 			glEnable(GL_LIGHTING);
 		}
 
-		for (t = 0; t < mesh->mNumFaces; ++t) {
+		for (t = 0; t < mesh->mNumFaces; ++t) 
+		{
 			const struct aiFace* face = &mesh->mFaces[t];
+			if( face == NULL )
+				continue;
 			GLenum face_mode;
 
-			switch(face->mNumIndices) {
+			switch(face->mNumIndices) 
+			{
 				case 1: face_mode = GL_POINTS; break;
 				case 2: face_mode = GL_LINES; break;
 				case 3: face_mode = GL_TRIANGLES; break;
@@ -401,7 +411,8 @@ void AssetObject::RecursiveRender (const struct aiScene *sc, const struct aiNode
 
 			glBegin(face_mode);
 
-			for(i = 0; i < face->mNumIndices; i++) {
+			for(i = 0; i < face->mNumIndices; i++) 
+			{
 				int index = face->mIndices[i];
 				if(mesh->mColors[0] != NULL)
 					glColor4fv((GLfloat*)&mesh->mColors[0][index]);
